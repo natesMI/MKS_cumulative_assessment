04e1c88e625e6a59e73477c83a01bafb704e0db0
Nathaniel Schwab <schwab.nathaniel@gmail.com>
2016-05-21 17:25:10 -0700

    Deployment troubleshooting

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ var app = express();
 app.use(express.static(__dirname + '/client'));
 
 var client = new Twitter({
-  consumer_key: credentials.keys.MY_CONSUMER_KEY || process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: credentials.keys.MY_CONSUMER_SECRET || process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: credentials.keys.MY_ACCESS_TOKEN || process.env.TWITTER_TOKEN_KEY,
-  access_token_secret: credentials.keys.MY_TOKEN_SECRET || process.env.TWITTER_TOKEN_SECRET
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_TOKEN_SECRET
 });
 
 app.get('/api/start', function(req, res){
